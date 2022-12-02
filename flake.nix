dfe474f9baaf97f7587598823cf60bde431d9c25
shifterbit <48891590+shifterbit@users.noreply.github.com>
2022-12-02 00:55:33 -0500

    setup nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        ghc
+        haskell-language-server
+      ];
+    };
+  };
+}
