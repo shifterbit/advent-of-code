0e415d50e2b9564c6fba628c4f987ff96ed064d5
shifterbit <48891590+shifterbit@users.noreply.github.com>
2022-12-02 00:56:43 -0500

    Day 1 Solutions

---
+++ 2022/day1/calorie.hs
@@ -0,0 +1,31 @@
+import Data.List (sort)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified System.IO as IO
+
+type Elf = [Int]
+
+highestCalorieCount :: [Elf] -> Int
+highestCalorieCount [] = 0
+highestCalorieCount xs = foldr (max . sum) 0 xs
+
+topThreeCalorieCounts :: [Elf] -> [Int]
+topThreeCalorieCounts xs = (take 3 . reverse . sort) (map sum xs)
+
+parseInput :: T.Text -> [[Int]]
+parseInput str = result
+  where
+    counts = T.splitOn (T.pack "\n\n") str
+    lists = map (lines . T.unpack) counts
+    result = map (fmap read) lists
+
+
+-- Part 1 Solution
+mostCalories path = do
+  contents <- TIO.readFile path
+  return $ (highestCalorieCount . parseInput) contents
+
+-- Part 2 Solution
+topThreeCalorieCountsCombined path = do
+  contents <- TIO.readFile path
+  return $ (sum . topThreeCalorieCounts . parseInput) contents
