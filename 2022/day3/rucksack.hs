375d27e0ef51fea545412d19fb7187bac3690aeb
shifterbit <48891590+shifterbit@users.noreply.github.com>
2022-12-08 22:28:18 -0500

    day 3 -> puzzle 2 solution

--- 2022/day3/rucksack.hs
+++ 2022/day3/rucksack.hs
@@ -1,10 +1,13 @@
-import Data.List (intersect, nub)
+import Control.Monad (foldM)
+import Data.List (foldl1', intersect, nub)
 import qualified Data.Map as M
 import System.IO (readFile')
 
 newtype Compartment = Compartment String deriving (Show)
 
 newtype Rucksack = Rucksack (Compartment, Compartment) deriving (Show)
+
+newtype Group = Group [Maybe Rucksack] deriving (Show)
 
 sampleInput =
   unlines
@@ -16,7 +19,7 @@ sampleInput =
       "CrZsJsPPZsGzwwsLwLmpwMDw"
     ]
 
-priorityTable = M.fromList $ zip ['a' .. 'z'] [1 .. 26] ++ zip ['A' .. 'Z'] [27 .. 52]
+priorityTable = M.fromList $ zip [' '] [0] ++ zip ['a' .. 'z'] [1 .. 26] ++ zip ['A' .. 'Z'] [27 .. 52]
 
 mkRucksack :: (String, String) -> Rucksack
 mkRucksack (a, b) = Rucksack (Compartment a, Compartment b)
@@ -28,12 +31,21 @@ parseInput input = map mkRucksack compartmentPairs
     splitHalf xs = splitAt (length xs `div` 2) xs
     compartmentPairs = map splitHalf splitInput
 
-main0 :: FilePath -> IO (Maybe Integer)
-main0 path = do
-    input <- readFile' path
-    return $ sumPriorities input
+main1 :: FilePath -> IO ()
+main1 path = do
+  input <- readFile' path
+  let result = (sumPriorities . duplicateCompartments . parseInput) input
+  print result
 
+main2 :: FilePath -> IO ()
+main2 path = do
+  input <- readFile' path
+  let result = (sumPriorities . groupBadges . groupRucksacks . parseInput) input
+  print result
 
+puzzleSolution1 = main1 "rucksacks.txt"
+
+puzzleSolution2 = main2 "rucksacks.txt"
 
 duplicateCompartment :: Rucksack -> Char
 duplicateCompartment (Rucksack (Compartment a, Compartment b)) = head (nub (a `intersect` b))
@@ -48,4 +60,29 @@ compartmentPriorities :: [Char] -> [Maybe Integer]
 compartmentPriorities = map compartmentPriority
 
 sumPriorities :: String -> Maybe Integer
-sumPriorities a = sum <$> (sequence . compartmentPriorities . duplicateCompartments . parseInput) a
+sumPriorities a = sum <$> (sequence . compartmentPriorities) a
+
+groupRucksacks :: [Rucksack] -> [Group]
+groupRucksacks [] = []
+groupRucksacks [a] = [Group [Just a, Nothing, Nothing]]
+groupRucksacks [a, b] = [Group [Just a, Just b, Nothing]]
+groupRucksacks (a : b : c : xs) = Group [Just a, Just b, Just c] : groupRucksacks xs
+
+rucksackToString :: Rucksack -> String
+rucksackToString (Rucksack (Compartment a, Compartment b)) = a ++ b
+
+rucksacksToStrings :: [Rucksack] -> [String]
+rucksacksToStrings = map rucksackToString
+
+groupToString :: Group -> Maybe [String]
+groupToString (Group rucksacks) = rucksacksToStrings <$> sequence rucksacks
+
+groupBadge :: Group -> Char
+groupBadge group = uniqueLetters groupStrings
+  where
+    groupStrings = groupToString group
+    uniqueLetters (Just a) = (head . nub) $ foldl1' intersect a
+    uniqueLetters Nothing = ' '
+
+groupBadges :: [Group] -> [Char]
+groupBadges = map groupBadge
