aaa119407e0ef8639a6ca25f4dce2f053f17240b
shifterbit <48891590+shifterbit@users.noreply.github.com>
2022-12-10 00:10:20 -0500

    add comments to day1 code

--- 2022/day1/Calorie.hs
+++ 2022/day1/Calorie.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.List (sort)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -5,28 +7,31 @@ import qualified System.IO as IO
 
 type Elf = [Int]
 
+-- | Finds the elf with the highest calorie count
 highestCalorieCount :: [Elf] -> Int
 highestCalorieCount [] = 0
 highestCalorieCount xs = foldr (max . sum) 0 xs
 
+-- | Get the three Elves with the highest calorie counts
 topThreeCalorieCounts :: [Elf] -> [Int]
 topThreeCalorieCounts xs = (take 3 . reverse . sort) (map sum xs)
 
-parseInput :: T.Text -> [[Int]]
+parseInput :: T.Text -> [Elf]
 parseInput str = result
   where
-    counts = T.splitOn (T.pack "\n\n") str
+    counts = T.splitOn "\n\n" str
     lists = map (lines . T.unpack) counts
     result = map (fmap read) lists
 
-
--- Part 1 Solution
+-- | Part 1 Solution:
+--   Reads Input from a file and Finds the Elf with the highest calorie count
 mostCalories :: FilePath -> IO Int
 mostCalories path = do
   contents <- TIO.readFile path
   return $ (highestCalorieCount . parseInput) contents
 
--- Part 2 Solution
+-- | Part 2 Solution
+--   Reads Input from a file and Finds the three Elves with the highest calorie counts
 topThreeCalorieCountsCombined :: FilePath -> IO Int
 topThreeCalorieCountsCombined path = do
   contents <- TIO.readFile path
